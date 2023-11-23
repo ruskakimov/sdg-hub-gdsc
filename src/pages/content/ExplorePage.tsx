1b974897811b68cd86bdefff84e4ac4a6a235d0e
Rustem Kakimov <ruskakimov@gmail.com>
2023-11-23 04:04:58 -0500

    Tags

--- src/pages/content/ExplorePage.tsx
+++ src/pages/content/ExplorePage.tsx
@@ -104,43 +104,47 @@ export default function ExplorePage() {
               </tr>
             </thead>
             <tbody className="divide-y divide-gray-200 bg-white">
-              {professors.map((person) => (
-                <tr key={person.email}>
+              {professors.map((professor) => (
+                <tr key={professor.email}>
                   <td className="whitespace-nowrap py-5 pl-4 pr-3 text-sm sm:pl-0">
                     <div className="flex items-center">
                       <div className="h-11 w-11 flex-shrink-0">
                         <img
                           className="h-11 w-11 rounded-full"
-                          src={person.imageUrl}
+                          src={professor.imageUrl}
                           alt=""
                         />
                       </div>
                       <div className="ml-4">
                         <div className="font-medium text-gray-900">
-                          {person.name}
+                          {professor.name}
                         </div>
-                        <div className="mt-1 text-gray-500">{person.email}</div>
+                        <div className="mt-1 text-gray-500">
+                          {professor.email}
+                        </div>
                       </div>
                     </div>
                   </td>
                   <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
-                    <div className="text-gray-900">{person.university}</div>
-                    <div className="mt-1 text-gray-500">{person.title}</div>
+                    <div className="text-gray-900">{professor.university}</div>
+                    <div className="mt-1 text-gray-500">{professor.title}</div>
+                  </td>
+                  <td className="whitespace-wrap flex-wrap max-w-xs align-top flex gap-2 px-3 py-5 text-sm text-gray-500">
+                    {professor.tags.map((tag) => (
+                      <span className="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
+                        {tag}
+                      </span>
+                    ))}
                   </td>
                   <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
-                    <span className="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
-                      Active
-                    </span>
-                  </td>
-                  <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
-                    {person.university}
+                    {professor.university}
                   </td>
                   <td className="relative whitespace-nowrap py-5 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
                     <a
                       href="#"
                       className="text-indigo-600 hover:text-indigo-900"
                     >
-                      Edit<span className="sr-only">, {person.name}</span>
+                      Edit<span className="sr-only">, {professor.name}</span>
                     </a>
                   </td>
                 </tr>
