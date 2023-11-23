bb258754a3ca4861cf3b95d5aef0c59cb82fca9e
Rustem Kakimov <ruskakimov@gmail.com>
2023-11-23 04:42:06 -0500

    Color tags

--- src/pages/content/ExplorePage.tsx
+++ src/pages/content/ExplorePage.tsx
@@ -41,6 +41,26 @@ const goals: string[] = [
   "Partnerships",
 ];
 
+const goalColors: string[][] = [
+  ["bg-red-50", "ring-red-600/20", "text-red-800"],
+  ["bg-amber-50", "ring-amber-600/20", "text-amber-800"],
+  ["bg-green-50", "ring-green-600/20", "text-green-800"],
+  ["bg-rose-50", "ring-rose-600/20", "text-rose-800"],
+  ["bg-orange-50", "ring-orange-600/20", "text-orange-800"],
+  ["bg-cyan-50", "ring-cyan-600/20", "text-cyan-800"],
+  ["bg-yellow-50", "ring-yellow-600/20", "text-yellow-800"],
+  ["bg-red-50", "ring-red-600/20", "text-red-800"],
+  ["bg-orange-50", "ring-orange-600/20", "text-orange-800"],
+  ["bg-pink-50", "ring-pink-600/20", "text-pink-800"],
+  ["bg-amber-50", "ring-amber-600/20", "text-amber-800"],
+  ["bg-orange-50", "ring-orange-600/20", "text-orange-800"],
+  ["bg-green-50", "ring-green-600/20", "text-green-800"],
+  ["bg-blue-50", "ring-blue-600/20", "text-blue-800"],
+  ["bg-lime-50", "ring-lime-600/20", "text-lime-800"],
+  ["bg-indigo-50", "ring-indigo-600/20", "text-indigo-800"],
+  ["bg-violet-50", "ring-violet-600/20", "text-violet-800"],
+];
+
 const professors: Professor[] = [
   {
     name: "Saad Bin Ahmed",
@@ -86,18 +106,21 @@ export default function ExplorePage() {
                 >
                   Institution
                 </th>
-                <th
-                  scope="col"
-                  className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-                >
-                  Research Areas
-                </th>
+
                 <th
                   scope="col"
                   className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
                 >
                   SDG Goals
                 </th>
+
+                <th
+                  scope="col"
+                  className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                >
+                  Research Areas
+                </th>
+
                 <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-0">
                   <span className="sr-only">Edit</span>
                 </th>
@@ -129,6 +152,21 @@ export default function ExplorePage() {
                     <div className="text-gray-900">{professor.university}</div>
                     <div className="mt-1 text-gray-500">{professor.title}</div>
                   </td>
+
+                  <td className="px-3 py-5 text-sm text-gray-500">
+                    <div className="flex flex-wrap gap-2">
+                      {professor.goals.map((goalNumber) => (
+                        <span
+                          className={`inline-flex items-center rounded-md px-2 py-1 text-xs font-medium ring-1 ring-inset ${goalColors[
+                            goalNumber
+                          ].join(" ")}`}
+                        >
+                          {goals[goalNumber - 1]}
+                        </span>
+                      ))}
+                    </div>
+                  </td>
+
                   <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
                     <div className="flex flex-wrap gap-2">
                       {professor.tags.map((tag) => (
@@ -138,15 +176,7 @@ export default function ExplorePage() {
                       ))}
                     </div>
                   </td>
-                  <td className="px-3 py-5 text-sm text-gray-500">
-                    <div className="flex flex-wrap gap-2">
-                      {professor.goals.map((goalNumber) => (
-                        <span className="inline-flex items-center rounded-md bg-blue-50 px-2 py-1 text-xs font-medium text-blue-700 ring-1 ring-inset ring-blue-600/20">
-                          {goals[goalNumber - 1]}
-                        </span>
-                      ))}
-                    </div>
-                  </td>
+
                   <td className="relative whitespace-nowrap py-5 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
                     <a
                       href="#"
